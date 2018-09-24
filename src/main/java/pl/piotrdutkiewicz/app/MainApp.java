83d1917d39d0ae6f26ca88e4746dc8fd256b31ee
Piotr <boleczi@gmail.com>
2018-09-24 20:28:56 +0200

    wstawion spacje2

--- src/main/java/pl/piotrdutkiewicz/app/MainApp.java
+++ src/main/java/pl/piotrdutkiewicz/app/MainApp.java
@@ -5,6 +5,6 @@ public class MainApp
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! " );
+        System.out.println( "Hello World!...   " );
     }
 }
