ac7456259ba7148abffa690361d1270d46cfb966
PiotrD <boleczi@gmail.com>
2018-09-25 00:07:47 +0200

    uruchomione resty

--- src/main/java/pl/piotrdutkiewicz/app/MainApp.java
+++ src/main/java/pl/piotrdutkiewicz/app/MainApp.java
@@ -1,10 +1,14 @@
 package pl.piotrdutkiewicz.app;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@SpringBootApplication
 
 public class MainApp 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    SpringApplication.run(MainApp.class, args);    
     }
 }
